11c1a6b4dd69830d9e0a1b8ede7a15b96df287b4
Brian MacLean <brian.maclean@verizon.net>
2020-04-26 22:00:44 -0700

    lesson 7

--- lessonSeven.py
+++ lessonSeven.py
@@ -4,18 +4,34 @@
 # need.
 def reverseStr(string) :
     # your code here
+    s = string
+    for i in s :
+        s += i
+    return s
 
 
 # Given 'num' find if the value is prime or not.
 # Remember to use '%' to find if something is divisible by something else.
 def findIfPrime(num) :
     # your code here
+  if num > 1 :
+    for i in range(2, num//2) :
+      if (num % i) == 0 :
+        return False
+    else :
+      return True
+  else :
+    return False
 
-
-# Given two numbers find the greatest Common Divisor between those two
+# Given two numbers, find the greatest Common Divisor between those two
 # numbers. If they are relatively prime, return -1.
 def greatestCommonDivisor(num1, num2) :
     # your code here
+    if (num2 == 0) :
+        return num1
+    else :
+        return greatestCommonDivisor(num2, num1 % num2)
+    
 
 
 print("Reverse String: ", reverseStr('apples'))
